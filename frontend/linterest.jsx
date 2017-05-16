0172095eab373722deb52e42416ec3747212f10d
Yong Lin <gmale88@gmail.com>
2017-05-16 12:03:24 -0700

    Create root reducer, add session reducer

--- frontend/linterest.jsx
+++ frontend/linterest.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>React is working</h1>, root);
+  const store = configureStore();
+  window.store = store;
+  ReactDOM.render(<h1>React is not working</h1>, root);
 });
 
+// TODO: remove after testing
 import * as APIUtil from './util/session_api_util';
 
 window.signup = APIUtil.signup;
