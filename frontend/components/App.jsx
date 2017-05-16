7870f3ddfc909b0b5d3c83a5456fbd18b498cbc1
Yong Lin <gmale88@gmail.com>
2017-05-16 14:54:20 -0700

    greetings and session_form components

---
+++ frontend/components/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import {
+  Route,
+  Redirect,
+  Switch,
+  Link,
+  HashRouter
+} from 'react-router-dom';
+
+import GreetingContainer from './greeting/greeting_container';
+import SessionFormContainer from './session_form/session_form_container';
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
+
+const App = () => (
+  <div>
+    <Link to="/" className="header-link">
+      <h1>Linterest</h1>
+    </Link>
+    <GreetingContainer />
+      <Switch>
+       <AuthRoute path="/login" component={SessionFormContainer} />
+       <AuthRoute path="/signup" component={SessionFormContainer} />
+     </Switch>
+  </div>
+);
+
+export default App;
--- frontend/linterest.jsx
+++ frontend/linterest.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
+import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   const store = configureStore();
   window.store = store;
-  ReactDOM.render(<h1>React is not working</h1>, root);
+  ReactDOM.render(<Root store={store}/>, root);
 });
 
 // TODO: remove after testing
-import * as APIUtil from './util/session_api_util';
+// import * as APIUtil from './util/session_api_util';
+//
+// window.signup = APIUtil.signup;
+// window.login = APIUtil.login;
+// window.logout = APIUtil.logout;
 
-window.signup = APIUtil.signup;
-window.login = APIUtil.login;
-window.logout = APIUtil.logout;
+import {signup, login, logout } from './actions/session_actions';
+
+window.signup = signup;
+window.login = login;
+window.logout = logout;
